a1d585b6cba16808d38c099c65a160fe32bdf2a3
Nicholas Humfrey <njh@aelius.com>
2016-07-07 22:51:45 +0100

    Removed the now unused ether_header and ip6_header definitions from packet_headers.h

--- packet_headers.h
+++ packet_headers.h
@@ -1,26 +1,6 @@
 #ifndef ETHERSIA_PACKET_HEADERS_H
 #define ETHERSIA_PACKET_HEADERS_H
 
-/*
- * Structure of a 10Mb/s Ethernet header.
- */
-struct ether_header {
-    MACAddress dest;
-    MACAddress src;
-    uint16_t type;
-} __attribute__((__packed__));
-#define ETHER_HEADER_LEN          (14)
-#define ETHER_HEADER_OFFSET       (0)
-#define ETHER_HEADER              ((struct ether_header*)(buffer + ETHER_HEADER_OFFSET))
-
-static_assert(sizeof(struct ether_header) == ETHER_HEADER_LEN, "Size is not correct");
-
-#define ETHER_TYPE_IPV6           0x86dd
-
-#define IP6_PROTO_TCP             6
-#define IP6_PROTO_UDP             17
-#define IP6_PROTO_ICMP6           58
-
 #define ICMP6_TYPE_ECHO           128
 #define ICMP6_TYPE_ECHO_REPLY     129
 #define ICMP6_TYPE_RS             133
@@ -37,35 +17,13 @@ static_assert(sizeof(struct ether_header) == ETHER_HEADER_LEN, "Size is not corr
 #define ICMP6_OPTION_MTU                 5
 
 
-/*
- * Definition for internet protocol version 6.
- * RFC 2460
- */
-
-struct ip6_header {
-    uint8_t  ver_tc;     // 4-bits of version, 4-bits of traffic class
-    uint8_t  tc_fl;      // 4-bits of traffic class, 4-bits of flow label
-    uint16_t flow_label; // Remaining 16-bits of 20-bit flow label
-    uint16_t length;     // Payload length (not including this header)
-    uint8_t  proto;      // Identifies the type of header immediately following
-    uint8_t  hop_limit;  // Decremented by 1 by each node that forwards the packet
-    IPv6Address src;
-    IPv6Address dest;
-} __attribute__((__packed__));
-#define IP6_HEADER_LEN            (40)
-#define IP6_HEADER_OFFSET         (ETHER_HEADER_OFFSET + ETHER_HEADER_LEN)
-#define IP6_HEADER                ((struct ip6_header*)(buffer + IP6_HEADER_OFFSET))
-
-static_assert(sizeof(struct ip6_header) == IP6_HEADER_LEN, "Size is not correct");
-
-
 struct icmp6_header {
     uint8_t type;
     uint8_t code;
     uint16_t checksum;
 } __attribute__((__packed__));
 #define ICMP6_HEADER_LEN          (4)
-#define ICMP6_HEADER_OFFSET       (IP6_HEADER_OFFSET + IP6_HEADER_LEN)
+#define ICMP6_HEADER_OFFSET       (ETHER_HEADER_LEN + IP6_HEADER_LEN)
 #define ICMP6_HEADER              ((struct icmp6_header*)(buffer + ICMP6_HEADER_OFFSET))
 
 static_assert(sizeof(struct icmp6_header) == ICMP6_HEADER_LEN, "Size is not correct");
