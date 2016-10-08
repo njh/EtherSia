11181075000836f75a35a74d8f91b2c08d90836c
Nicholas Humfrey <njh@aelius.com>
2016-10-08 14:58:37 +0100

    Tied up and finished documenting the TCPServer class

--- src/TCPServer.h
+++ src/TCPServer.h
@@ -12,7 +12,14 @@
 class EtherSia;
 
 /**
- * Class for sending and receiving TCP packets on a specified port
+ * Class for responding to TCP requests
+ *
+ * Requests and responses cannot be bigger than a single packet and
+ * are limited by the size of the packet buffer.
+ *
+ * This class inherits from Print, so you you can also use the print() 
+ * and println() functions when composing a reply.
+ *
  */
 class TCPServer : public Print {
 
@@ -33,19 +40,39 @@ public:
     uint16_t localPort();
 
     /**
-     * Check if a TCP packet is available to be processed on this server
-     * @return true if there is a valid packet has been received for this server
+     * Check if a TCP data packet is available to be processed for this server
+     *
+     * This method also has a side effect of responding to other stages
+     * of the TCP sequence (the SYN and FIN packets).
+     *
+     * @return true if there is a valid packet with data has been received for this server
      */
     boolean havePacket();
 
-
-
+    /**
+     * Reply to a TCP request. The response should have been written to the 
+     * packet buffer using the print() and println() methods.
+     */
     void sendReply();
 
+    /**
+     * Reply to a TCP request with a null-terminated string
+     *
+     * The string is copied into the packet buffer before sending.
+     *
+     * @param string The string to place in the reply
+     */
     void sendReply(const char *string);
 
+    /**
+     * Reply to a TCP request with a buffer
+     *
+     * The bytes are copied into the packet buffer before sending.
+     *
+     * @param data The data to reply with
+     * @param len The length of the data in bytes
+     */
     void sendReply(const void* data, uint16_t len);
-
 
     /**
      * Get the IPv6 source address of the last TCP packet received
@@ -113,30 +140,35 @@ public:
     virtual size_t write(uint8_t chr);
 
 protected:
+    enum TCP_FLAGS {
+        TCP_FLAG_URG = 0x20,  ///< Urgent Pointer field is significant
+        TCP_FLAG_ACK = 0x10,  ///< Acknowledgment field is significant
+        TCP_FLAG_PSH = 0x08,  ///< Push function
+        TCP_FLAG_RST = 0x04,  ///< Reset the connection
+        TCP_FLAG_SYN = 0x02,  ///< Synchronise sequence numbers
+        TCP_FLAG_FIN = 0x01   ///< No more data from sender
+    };
 
+    /**
+     * Reply to a TCP packet, using data contained in buffer
+     *
+     * @param len The length of the data in the buffer
+     * @param flags The flags to set on the outgoing packet (@ref TCP_FLAGS)
+     * @return The number of bytes written to the buffer
+     */
     void sendReplyWithFlags(uint16_t len, uint8_t flags);
 
-    EtherSia &_ether;            ///< The Ethernet Interface that this TCP server is attached to
-    uint16_t _localPort;         ///< The IPv6 local port number
+    /** The Ethernet Interface that this TCP server is attached to */
+    EtherSia &_ether;
+    
+    /** The TCP local port number */
+    uint16_t _localPort;         
 
-    //IPv6Address _remoteAddress;  ///< The IPv6 remote address
-    //uint16_t _remotePort;        ///< The IPv6 remote port number
-
+    /** The current position of writing data to buffer (when using Print interface) */
     int16_t _responsePos;
 };
 
 
-#define TCP_FLAG_URG   (0x20)
-#define TCP_FLAG_ACK   (0x10)
-#define TCP_FLAG_PSH   (0x08)
-#define TCP_FLAG_RST   (0x04)
-#define TCP_FLAG_SYN   (0x02)
-#define TCP_FLAG_FIN   (0x01)
-
-enum TCP_STATES {
-  TCP_LISTEN,
-  TCP_SYN_RECEIVED,
-};
 
 
 /**
@@ -159,15 +191,17 @@ struct tcp_header {
 } __attribute__((__packed__));
 
 /**
- * The length of a TCP packet header
+ * The length of a TCP response packet header
  * @private
  */
 #define TCP_HEADER_LEN            (24)
 
+/**
+ * The maximum size of the TCP segment that we can recieve
+ * @private
+ */
 #define TCP_WINDOW_SIZE           (ETHERSIA_MAX_PACKET_SIZE - \
                                    ETHER_HEADER_LEN - IP6_HEADER_LEN - TCP_HEADER_LEN - 32)
-
-//#define TCP_WINDOW_SIZE           (1024)
 
 /**
  * Get the pointer to the TCP header from within EtherSia
