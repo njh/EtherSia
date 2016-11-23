2de04416bb32f2aba4b1988ce988fae9f8192e04
Nicholas Humfrey <njh@aelius.com>
2016-11-23 18:58:58 +0000

    Made the F() macro return a __FlashStringHelper

--- tests/libarduino/Progmem.h
+++ tests/libarduino/Progmem.h
@@ -1,15 +1,15 @@
 #ifndef Progmem_h
 #define Progmem_h
 
+class __FlashStringHelper;
+
 #define PROGMEM
-#define F(x) x
+#define F(x) (reinterpret_cast<const __FlashStringHelper *>(x))
 #define pgm_read_byte_near(x) *(x)
 #define memcpy_P(dst, src, n) memcpy(dst, src, n)
 #define memcmp_P(p1, p2, n) memcmp(p1, p2, n)
 #define strcmp_P(s1, s2) strcmp(s1, s2)
 #define strlen_P(str) strlen(str)
-#define pgm_read_byte(addr) *(addr);
-
-class __FlashStringHelper;
+#define pgm_read_byte(addr) *(addr)
 
 #endif
