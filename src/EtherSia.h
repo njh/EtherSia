1f52e3de895a82750a90d5497be3c9b8050c3617
Nicholas Humfrey <njh@aelius.com>
2017-02-27 14:13:36 +0000

    Changed receivePacket() to return 0 for ICMP packets handled internally

--- src/EtherSia.h
+++ src/EtherSia.h
@@ -377,7 +377,7 @@ protected:
      *
      * Responds to ICMPv6 Pings, Neighbour Solicitations and Router Solicitations
      */
-    void icmp6ProcessPacket();
+    boolean icmp6ProcessPacket();
 
     /**
      * Perform Stateless auto-configuration
