e27b09e765529a34a56b3d6336b62a9ab146042b
blocktrron <blocktrron@users.noreply.github.com>
2017-09-27 19:13:36 +0200

    fix ignoring icmp option

--- src/icmp6.cpp
+++ src/icmp6.cpp
@@ -172,14 +172,6 @@ void EtherSia::icmp6ProcessRA()
         return;
     }
 
-    if(_prefixRestrictionEnabled == true) {
-        struct icmp6_prefix_information *icmp_p = (struct icmp6_prefix_information*)&ptr[2];
-        IPv6Prefix advertised_prefix(&icmp_p->prefix, icmp_p->prefix_length);
-        if(!_whitelistedPrefix.contains(&advertised_prefix)) {
-            return;
-        }
-    }
-
     while(remaining > 0) {
         switch(ptr[0]) {
         case ICMP6_OPTION_SOURCE_LINK_ADDRESS:
@@ -187,6 +179,15 @@ void EtherSia::icmp6ProcessRA()
             _routerMac = *((MACAddress*)&ptr[2]);
             break;
         case ICMP6_OPTION_PREFIX_INFORMATION:
+            if(_prefixRestrictionEnabled == true) {
+                struct icmp6_prefix_information *icmp_p = (struct icmp6_prefix_information*)&ptr[2];
+                IPv6Prefix advertised_prefix(&icmp_p->prefix, icmp_p->prefix_length);
+                if(!_whitelistedPrefix.contains(&advertised_prefix)) {
+                    _whitelistedPrefix.println();
+                    advertised_prefix.println();
+                    return;
+                }
+            }
             icmp6ProcessPrefix(
                 (struct icmp6_prefix_information*)&ptr[2]
             );
